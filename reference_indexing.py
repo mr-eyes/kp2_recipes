a9c690b4033bf0d790a91a3d2fdcd7cb0e73a549
mr-eyes <mabuelanin@gmail.com>
2022-06-08 05:48:51 +0200

    reference indexing

--- reference_indexing.py
+++ reference_indexing.py
@@ -2,15 +2,6 @@ import kProcessor as kp
 from helper_functions import KFIter
 import sys
 
-
-"""
-/// Index function without needing the kmerDecoder
-    void
-    index(kDataFrame *frame, std::map<std::string, int> parse_params, string filename, int chunks,
-          string names_fileName);
-
-"""
-
 kf = kp.kDataFrameFactory.createPHMAP(31)
 filename = sys.argv[1]
 chunk_size = 100
